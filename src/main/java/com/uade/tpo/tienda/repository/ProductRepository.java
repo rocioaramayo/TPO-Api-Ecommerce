ed924cefb387e28b8f50dfbe97369703cd67280b
tomasbonomo <44001214+tomasbonomo@users.noreply.github.com>
2025-04-07 20:14:40 -0300

    creacion de ProductRepository

---
+++ src/main/java/com/uade/tpo/tienda/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.uade.tpo.tienda.repository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.uade.tpo.tienda.entity.Producto;
+
+@Repository
+
+public interface ProductRepository extends JpaRepository<Producto, Long> {
+
+    
+} 
