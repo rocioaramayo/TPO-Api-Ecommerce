ee95bbd8a75ef883a8f67d3f6f88f141247d99e0
tomasbonomo <44001214+tomasbonomo@users.noreply.github.com>
2025-04-14 20:35:47 -0300

    implementacion de capa de seguridad en log in y registro
    faltaria eliminar usuario de control y crear ususario

--- src/main/java/com/uade/tpo/tienda/controllers/AuthController.java
+++ src/main/java/com/uade/tpo/tienda/controllers/AuthController.java
@@ -1,7 +1,9 @@
 package com.uade.tpo.tienda.controllers;
 
+import com.uade.tpo.tienda.dto.AuthenticationResponse;
 import com.uade.tpo.tienda.dto.LoginRequest;
 import com.uade.tpo.tienda.dto.LoginResponse;
+import com.uade.tpo.tienda.dto.RegisterRequest;
 import com.uade.tpo.tienda.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,10 @@ public class AuthController {
 
     private final AuthService authService;
 
+        @PostMapping("/register")
+public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+    return ResponseEntity.ok(authService.register(request));
+}
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
