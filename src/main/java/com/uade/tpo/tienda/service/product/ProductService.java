c8ec2c17607f3cb1ed1d82a84c16e91be5f015b0
rocioaramayo <116124063+rocioaramayo@users.noreply.github.com>
2025-04-15 20:30:55 -0300

    INCORPORACION de endpoint filtrado y verdetalle, actualizacion de permisos
    Co-Authored-By: tomasbonomo <44001214+tomasbonomo@users.noreply.github.com>

--- src/main/java/com/uade/tpo/tienda/service/product/ProductService.java
+++ src/main/java/com/uade/tpo/tienda/service/product/ProductService.java
@@ -1,6 +1,10 @@
 package com.uade.tpo.tienda.service.product;
 
 import com.uade.tpo.tienda.entity.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -9,4 +13,8 @@ public interface ProductService {
   Page<Producto> getProducts(Pageable pageable);
   Producto updateProduct(Long id, Producto productoUpdated);
   void deleteProduct(Long id);
+  Optional<Producto> getProductById(Long id);
+  List<Producto> filtrarProductos(String nombre, String categoria, Double precioMax);
+
+
 }
