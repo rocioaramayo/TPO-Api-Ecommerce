bcab66b422f4ffd0b886f84b9f1b11f4440406af
rocioaramayo <116124063+rocioaramayo@users.noreply.github.com>
2025-05-28 18:22:02 -0300

    Saque enpoint q permitia cargar imagenes sueltas

--- src/main/java/com/uade/tpo/tienda/controllers/ImagesController.java
+++ src/main/java/com/uade/tpo/tienda/controllers/ImagesController.java
@@ -2,7 +2,7 @@ package com.uade.tpo.tienda.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.uade.tpo.tienda.dto.AddFileRequest;
+
 import com.uade.tpo.tienda.dto.ImageResponse;
 import com.uade.tpo.tienda.entity.FotoProducto;
 import com.uade.tpo.tienda.service.image.ImageService;
@@ -11,13 +11,13 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Base64;
 
-import javax.sql.rowset.serial.SerialException;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -45,24 +45,5 @@ public class ImagesController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping()
-    public ResponseEntity<String> addImagePost(AddFileRequest request) throws IOException, SerialException, SQLException {
-        // Validar que el archivo no esté vacío
-        if (request.getFile().isEmpty()) {
-            return ResponseEntity.badRequest().body("El archivo está vacío");
-        }
-        
-        // Convertir archivo a Blob
-        byte[] bytes = request.getFile().getBytes();
-        javax.sql.rowset.serial.SerialBlob blob = new javax.sql.rowset.serial.SerialBlob(bytes);
-        
-        // Crear FotoProducto
-        FotoProducto foto = FotoProducto.builder()
-                .image(blob)
-                .build();
-                
-        imageService.create(foto);
-        
-        return ResponseEntity.ok("created");
-    }
+
 }
